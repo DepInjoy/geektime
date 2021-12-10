b7b7ac6903e51f20c3b0598a2368a81d6c4f286f
DepInjoy <xieyue28@163.com>
2021-12-10 21:51:22 +0800

    选择排序

--- algorithm/Sort/Inner_Sort.cpp
+++ algorithm/Sort/Inner_Sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 /*
     内部排序算法实现
@@ -98,13 +99,41 @@ void Sedgewick_Shell_Sort(T data[], const int N) {
     }
 }
 
+template<typename T>
+int ScanForMinIndex(T data[], int start, int end) {
+    int index = start;
+    for (int i = start; i < end; ++i) {
+        if (data[i] < data[index]) {
+            index = i;
+        }
+    }
+    return index;
+}
+
+/**
+ * @brief       选择排序实现
+ * 
+ * @tparam T 
+ * @param data 
+ * @param N 
+ */
+template<typename T>
+void Selection_Sort(T data[], const int N) {
+    for (int i = 0; i < N; ++i) {
+        // 寻找未排序部分(data[i]到data[N-1])中最小值所在位置,记为min_index
+        int min_index = ScanForMinIndex(data, i, N - 1);
+        // 将未排序部分的最小元换到有序部分的最后位置
+        Swap(data[min_index], data[i]);
+    }
+}
+
 int main(int argc, char* argv[]) {
     const int N = 11;  // 用于验证的数据的个数
     int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};  // 用于验证的数组
     std::cout << "Bubble Sort:" << std::endl;
     Bubble_Sort(array, N);
     printData(array, N);
-    
+
     std::cout << "Inserter Sort:" << std::endl;
     Inserter_Sort(array, N);
     printData(array, N);
@@ -115,6 +144,10 @@ int main(int argc, char* argv[]) {
 
     std::cout << "Sedgewick Shell Sort:" << std::endl;
     Sedgewick_Shell_Sort(array, N);
+    printData(array, N);
+
+    std::cout << "Selection Shell Sort:" << std::endl;
+    Selection_Sort(array, N);
     printData(array, N);
     return 0;
 }
