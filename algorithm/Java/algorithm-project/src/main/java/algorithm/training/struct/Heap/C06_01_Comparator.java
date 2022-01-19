eaf928e6a065c94172ef6bd2e68ab00231d2a0ef
DepInjoy <xieyue28@163.com>
2022-01-19 20:17:10 +0800

    Java比较器实现

--- algorithm/Java/algorithm-project/src/main/java/algorithm/training/struct/Heap/C06_01_Comparator.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/training/struct/Heap/C06_01_Comparator.java
@@ -1,5 +1,9 @@
 package algorithm.training.struct.Heap;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Comparator;
+
 /**
  * @brief:      比较器，实质就是重载比较运算符
  *                  1. 比较器可以很好的应用在特殊标准的排序上
@@ -14,4 +18,54 @@ package algorithm.training.struct.Heap;
  *                      返回0的情况，就是o1与o2同样优先的情况
  * */
 public class C06_01_Comparator {
+    static class Student {
+        public Integer id;
+        public Integer age;
+        public String name;
+
+        public Student(String name, Integer id, Integer age) {
+            this.id = id;
+            this.age = age;
+            this.name = name;
+        }
+    };
+
+    static class IdAscendingOrder implements Comparator<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return s1.id - s2.id;
+        }
+    }
+
+    static class IdAndAgeAscendingOrder implements Comparator<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return (s1.id - s2.id != 0) ? s1.id - s2.id : s1.age - s2.age;
+        }
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("A", 4, 40);
+        Student student2 = new Student("B", 4, 21);
+        Student student3 = new Student("C", 3, 12);
+        Student student4 = new Student("D", 3, 62);
+        Student student5 = new Student("E", 3, 42);
+
+        System.out.println("按照ID排序:");
+        Student[] students1 = new Student[]{student1, student2, student3, student4, student5 };
+        Arrays.sort(students1, new IdAscendingOrder());
+        for (int i = 0; i < students1.length; i++) {
+            Student s = students1[i];
+            System.out.println(s.name + "," + s.id + "," + s.age);
+        }
+
+        System.out.println("先按照ID排序，如果ID相同则按照Age排序:");
+        Student[] students2 = new Student[]{student1, student2, student3, student4, student5 };
+        Arrays.sort(students2, new IdAndAgeAscendingOrder());
+        for (int i = 0; i < students2.length; i++) {
+            Student s = students2[i];
+            System.out.println(s.name + "," + s.id + "," + s.age);
+        }
+    }
+
 }
