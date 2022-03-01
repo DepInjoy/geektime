c545769bc1c0107d9109490b3c2558176aa387ba
DepInjoy <xieyue28@163.com>
2022-03-01 15:54:48 +0800

    DP练习

--- algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D17_04_PrintAllPermutations.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D17_04_PrintAllPermutations.java
@@ -1,7 +1,114 @@
 package algorithm.training.algorithm.DP;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @brief:      打印一个字符串的全部子序列，要求不要出现重复字面值的子序列
+ * @brief:      打印一个字符串的全部排列(要求不要出现重复的排列)
  * */
 public class D17_04_PrintAllPermutations {
+    // 打印一个字符串的全排列
+    public static List<String> permutation1(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.length() == 0) return ans;
+
+        char[] str = s.toCharArray();
+        process1(str,0, ans);
+        return ans;
+    }
+
+    // 打印一个字符串的全排列
+    public static List<String> permutation2(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.length() == 0) return ans;
+
+        ArrayList<Character> rest = new ArrayList<>();
+        for (char ch : s.toCharArray()) {
+            rest.add(ch);
+        }
+        process2(rest, ans, "");
+        return ans;
+    }
+
+    // 打印一个字符串的不出现重复的全部排列
+    public static List<String> permutation3NoReapate(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.length() == 0) return ans;
+
+        char[] str = s.toCharArray();
+        process3(str, 0, ans);
+        return ans;
+    }
+
+    public static void process1(char[] str, int index, List<String> ans) {
+        if (index == str.length) {
+            ans.add(String.valueOf(str));
+            return;
+        }
+
+        for (int i = index; i < str.length; ++i) {
+            swap(str, index, i);
+            process1(str, index + 1, ans);
+            swap(str, index, i);
+        }
+    }
+
+    public static void process2(ArrayList<Character> rest, List<String> ans, String path) {
+        if (rest.isEmpty()) {
+            ans.add(path);
+            return;
+        }
+
+        int N = rest.size();
+        for (int i = 0; i < N; i++) {
+            Character cur = rest.get(i);
+            rest.remove(i);
+            process2(rest, ans, path + String.valueOf(cur));
+            rest.add(i, cur);
+        }
+    }
+
+    public static void process3(char[] str, int index, List<String> ans) {
+        if (index == str.length) {
+            ans.add(String.valueOf(str));
+            return;
+        }
+
+        boolean[] visited = new boolean[256];
+        for (int i = index; i < str.length; ++i) {
+            if (!visited[str[i]]) {
+                visited[str[i]] = true;
+                swap(str, index, i);
+                process3(str, index + 1, ans);
+                swap(str, index, i);
+            }
+        }
+    }
+
+    public static void swap(char[] str, int i, int j) {
+        char tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        String s = "acc";
+        List<String> ans1 = permutation1(s);
+        for (String str : ans1) {
+            System.out.println(str);
+        }
+
+        System.out.println("====================");
+        List<String> ans2 = permutation2(s);
+        for (String str : ans2) {
+            System.out.println(str);
+        }
+
+
+        System.out.println("====================");
+        List<String> ans3 = permutation3NoReapate(s);
+        for (String str : ans3) {
+            System.out.println(str);
+        }
+    }
 }
--- algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D17_05_ReverseStackUsingRecursive.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D17_05_ReverseStackUsingRecursive.java
@@ -1,8 +1,41 @@
 package algorithm.training.algorithm.DP;
 
+import java.util.Stack;
+
 /**
  * @brief:      仰望好的尝试
  *              给你一个栈，实现逆序这个栈，不能申请额外的数据结构，只能使用递归函数
  * */
 public class D17_05_ReverseStackUsingRecursive {
+    public static void reverse(Stack<Integer> stack) {
+        if (stack.isEmpty()) return;
+
+        int last = process(stack);
+        reverse(stack);
+        stack.push(last);
+    }
+
+    public static int process(Stack<Integer> stack) {
+        int ans = stack.pop();
+        if (stack.isEmpty()) {
+            return ans;
+        } else {
+            int last = process(stack);
+            stack.push(ans);
+            return last;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> test = new Stack<Integer>();
+        test.push(1);
+        test.push(2);
+        test.push(3);
+        test.push(4);
+        test.push(5);
+        reverse(test);
+        while (!test.isEmpty()) {
+            System.out.println(test.pop());
+        }
+    }
 }
