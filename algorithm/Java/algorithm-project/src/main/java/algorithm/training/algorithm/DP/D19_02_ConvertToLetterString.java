c9c6ce002effe0bd07b7ba184d3351b360bf37bb
DepInjoy <xieyue28@163.com>
2022-03-04 21:24:30 +0800

    2DP

--- algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D19_02_ConvertToLetterString.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D19_02_ConvertToLetterString.java
@@ -7,4 +7,70 @@ package algorithm.training.algorithm.DP;
  *              给定一个只有数字字符组成的字符串str，返回有多少种转化结果
  * */
 public class D19_02_ConvertToLetterString {
+    public static int number(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        return process(str.toCharArray(), 0);
+    }
+
+    public static int process(char[] arr, int index) {
+        if (index == arr.length) return 1;
+        if (arr[index] == '0') return 0;
+
+        int ways = process(arr, index + 1);
+        if (index + 1 < arr.length &&
+                (arr[index] - '0') * 10 + (arr[index + 1] - '0') < 27) {
+            ways += process(arr, index+2);
+        }
+        return ways;
+    }
+
+    public static int number2(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int N = s.length();
+        int[] dp = new int[N+1];
+        dp[N] = 1;
+        char[] str = s.toCharArray();
+        for (int i = N - 1; i >= 0; i--) {
+            if (str[i] != '0') {
+                dp[i] = dp[i + 1];
+                if (i + 1 < N && ((str[i] - '0') * 10 + (str[i + 1] - '0')) < 27) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
+    }
+    /********************** For Test ****************************/
+    public static String randomString(int len) {
+        char[] str = new char[len];
+        for (int i = 0; i < len; i++) {
+            str[i] = (char) ((int) (Math.random() * 10) + '0');
+        }
+        return String.valueOf(str);
+    }
+
+    public static void main(String[] args) {
+        int N = 30;
+        int testTime = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int len = (int) (Math.random() * N);
+            String s = randomString(len);
+            int ans0 = number(s);
+            int ans2 = number2(s);
+            if (ans0 != ans2) {
+                System.out.println(s);
+                System.out.println(ans0);
+                System.out.println(ans2);
+                System.out.println("Oops!");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
 }
--- algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D20_01_PalindromeSubsequence.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D20_01_PalindromeSubsequence.java
@@ -4,6 +4,60 @@ package algorithm.training.algorithm.DP;
  * @brief:      给定一个字符串str，返回这个字符串的最长回文子序列长度
  *              比如 ： str = “a12b3c43def2ghi1kpm”
  *              最长回文子序列是“1234321”或者“123c321”，返回长度7
+ *
+ *              bbbab
+ *              L R 0   1   2   3   4
+ *              0   1   2
+ *              1       1   2   x2  x3
+ *              2           1   1   x1
+ *              3               1   1
+ *              4                   1
+ *
+ *              https://leetcode-cn.com/problems/longest-palindromic-subsequence/
  * */
 public class D20_01_PalindromeSubsequence {
+    public static int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] str = s.toCharArray();
+        return process(str, 0, str.length - 1);
+    }
+
+    public static int process(char[] str, int L, int R) {
+        if (L == R) return 1;
+        if (L == R - 1) return str[L] == str[R] ? 2 : 1;
+
+        int p1 = process(str, L + 1, R - 1);
+        int p2 = process(str, L + 1, R);
+        int p3 = process(str, L,  R - 1);
+        int p4 = str[L] == str[R] ? 2 + process(str, L + 1, R - 1) : 0;
+        return Math.max(Math.max(p1, p2), Math.max(p3, p4));
+    }
+
+    public static int longestPalindromeSubseq1(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] str = s.toCharArray();
+        int N = str.length;
+        int[][] dp = new int[N][N];
+        dp[N-1][N-1] = 1;
+        for (int i = 0; i < N-1; i++) {
+            dp[i][i] = 1;
+            dp[i][i+1] = str[i] == str[i+1] ? 2 : 1;
+        }
+
+        for (int i = N - 3; i >= 0; i--) {
+            for (int j = i + 2; j < N; j++) {
+                int p1 = dp[i+1][j-1];
+                int p4 = str[i] == str[j] ? 2 + dp[i+1][j-1] : 0;
+                dp[i][j] = Math.max(Math.max(dp[i+1][j-1], dp[i+1][j]),
+                        Math.max(dp[i][j-1], p4));
+            }
+        }
+        return dp[0][N-1];
+    }
 }
