0c7ed579899b92a6d2ebbb762952b6354111d0f7
DepInjoy <xieyue28@163.com>
2022-03-10 19:27:22 +0800

    DP练习题

--- algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D19_04_LongestCommonSubsequence.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/training/algorithm/DP/D19_04_LongestCommonSubsequence.java
@@ -1,4 +1,13 @@
 package algorithm.training.algorithm.DP;
-
+/**
+ * @brief:      给定两个字符串str1和str2, 返回这两个字符串的最长公共子序列长度
+ *              比如 ： str1 = “a12b3c456d”,str2 = “1ef23ghi4j56k”
+ *              最长公共子序列是“123456”，所以返回长度6
+ *
+ *              https://leetcode.com/problems/longest-common-subsequence/
+ * */
 public class D19_04_LongestCommonSubsequence {
+    public static int longestCommonSubsequence1(String s1, String s2) {
+        return 0;
+    }
 }
