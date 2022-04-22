91ae76d950bbecb88cc6f2adced9bc3a399867e2
DepInjoy <xieyue28@163.com>
2022-04-22 18:51:30 +0800

    2练习题

--- algorithm/Java/algorithm-project/src/main/java/algorithm/practice/dc/week3/L03_06_ClosestSubsequenceSum.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/practice/dc/week3/L03_06_ClosestSubsequenceSum.java
@@ -1,11 +1,38 @@
 package algorithm.practice.dc.week3;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @brief:  Leetcode 最接近sum的子序列累加和问题
  *          https://leetcode-cn.com/problems/closest-subsequence-sum/
  * */
 public class L03_06_ClosestSubsequenceSum {
     public int minAbsDifference(int[] nums, int goal) {
-        return 0;
+        if (nums == null || nums.length == 0) return -1;
+
+        int[] lSubSum = new int[1 >> 20];
+        int[] rSubSum = new int[1 >> 20];
+        int lend = process(nums, 0, nums.length << 1, 0, 0, lSubSum);
+        int rend = process(nums, nums.length << 1, nums.length, 0, 0, rSubSum);
+        Arrays.sort(lSubSum, 0, lend);
+        Arrays.sort(rSubSum, 0, rend--);
+        int ans = Math.abs(goal);
+        for(int i = 0; i < lend; i++) {
+            int rest = goal - lSubSum[i];
+            while (rend >= 0 && Math.abs(rest - rSubSum[rend - 1]) < Math.abs(rest - rSubSum[rend])) rend--;
+            ans = Math.min(ans, Math.abs(rest - rSubSum[rend]));
+        }
+        return ans;
+    }
+
+    public int process(int[] nums, int index, int end, int fillIndex, int sum, int[] arr) {
+        if (index == end) {
+            arr[fillIndex++] = sum;
+        } else {
+            fillIndex = process(nums, index + 1, end, fillIndex, sum, arr);
+            fillIndex = process(nums, index + 1, end, fillIndex, nums[index] + sum, arr)
+        }
+        return fillIndex;
     }
 }
--- algorithm/Java/algorithm-project/src/main/java/algorithm/practice/dc/week3/L03_07_FreedomTrail.java
+++ algorithm/Java/algorithm-project/src/main/java/algorithm/practice/dc/week3/L03_07_FreedomTrail.java
@@ -1,5 +1,9 @@
 package algorithm.practice.dc.week3;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @brief       自由之路
  *              https://leetcode.com/problems/freedom-trail/
@@ -7,6 +11,48 @@ package algorithm.practice.dc.week3;
 
 public class L03_07_FreedomTrail {
     public int findRotateSteps(String ring, String key) {
-        return 0;
+        if (ring == null || ring.length() == 0 || key == null || key.length() == 0) return 0;
+
+        HashMap<Character, ArrayList<Integer>> dialmap = new HashMap<>();
+        char[] ringChs = ring.toCharArray();
+        for (int i = 0; i < ringChs.length; i++) {
+            if (dialmap.containsKey(ringChs[i])) {
+                dialmap.get(ringChs[i]).add(i);
+            } else {
+                dialmap.put(ringChs[i], new ArrayList<>(i));
+            }
+        }
+
+        char[] keyChs = key.toCharArray();
+        int N = ring.length(), M = keyChs.length;
+        int [][] dp = new int[N][M+1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= M; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return process(dialmap, 0, 0, keyChs, ring.length(), dp);
+    }
+
+    public int process(HashMap<Character, ArrayList<Integer>> dialmap, int preButton, int index,
+                       char[] keyChs, int N, int[][] dp) {
+        if (dp[preButton][index] != -1) return dp[preButton][index];
+
+        int ans = Integer.MAX_VALUE;
+        if (index == keyChs.length) {
+            ans = 0;
+        } else {
+            for (Integer next : dialmap.get(keyChs[index])) {
+                ans = Math.min(ans, dialLength(preButton, next, N) +
+                        process(dialmap, next, index + 1, keyChs, N, dp));
+            }
+        }
+        dp[preButton][index] = ans;
+        return ans;
+    }
+
+    public int dialLength(int from, int to, int size) {
+        return Math.min(Math.abs(from - to), Math.abs(Math.min(from, to) + size -
+                Math.max(from, to)));
     }
 }
