65ff4fcc76c87aa2b6a4ecf7c2f7fc95bb46557b
DepInjoy <xieyue28@163.com>
2022-04-22 19:55:26 +0800

    完善描述

--- algorithm/CPP/topic/DoublePointer/L11_ContainerWithMostWater.cpp
+++ algorithm/CPP/topic/DoublePointer/L11_ContainerWithMostWater.cpp
@@ -1,7 +1,8 @@
 /**
- * @brief   给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i])
+ * @brief   给定一个长度为n的整数数组 height。有n条垂线，第i条线的两个端点是(i, 0)和(i, height[i])
  *          找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
  *          返回容器可以储存的最大水量。
+ * 
  *          链接：https://leetcode-cn.com/problems/container-with-most-water
  */
 
