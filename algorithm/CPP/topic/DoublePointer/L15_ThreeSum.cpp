098a351860e8151dd9cf5e1f979043a2dcdf5790
DepInjoy <xieyue28@163.com>
2022-04-22 08:01:03 +0800

    双指针练习题

---
+++ algorithm/CPP/topic/DoublePointer/L15_ThreeSum.cpp
@@ -0,0 +1,17 @@
+/**
+ * @brief   给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c
+ *          使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组
+ *          
+ *          链接：https://leetcode-cn.com/problems/3sum
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<vector<int>> threeSum(vector<int>& nums) {
+
+}
+
+int main() {
+    return 0;
+}
