a8ad4d425ff49ed264cb070ac3fc460e51816b35
DepInjoy <xieyue28@163.com>
2022-04-24 23:50:33 +0800

    准备练习题

---
+++ algorithm/CPP/topic/DP/L5_LongestPalindrome.cpp
@@ -0,0 +1,12 @@
+/**
+ * @brief   给你一个字符串 s，找到 s 中最长的回文子串
+ *          链接：https://leetcode-cn.com/problems/longest-palindromic-substring/
+ */
+
+#include<string>
+
+using namespace std;
+
+string longestPalindrome(string s) {
+
+}
---
+++ algorithm/CPP/topic/DP/L70_ClimbStairs.cpp
@@ -0,0 +1,9 @@
+/**
+ * @brief   假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *          每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *          链接：https://leetcode-cn.com/problems/climbing-stairs/
+ */
+
+int climbStairs(int n) {
+
+}
