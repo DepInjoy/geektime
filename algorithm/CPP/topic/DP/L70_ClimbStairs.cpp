18861a25a478ae9c814eeb3e939dec7bdba56683
DepInjoy <xieyue28@163.com>
2022-04-26 00:00:51 +0800

    1DP+2双指针练习题

--- algorithm/CPP/topic/DP/L70_ClimbStairs.cpp
+++ algorithm/CPP/topic/DP/L70_ClimbStairs.cpp
@@ -2,10 +2,33 @@
  * @brief   假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  *          每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  *          链接：https://leetcode-cn.com/problems/climbing-stairs/
+ * 
+ *          实现方式：
+ *              1. 暴力递归，有两种尝试方式，其一是从底走到顶，其二是从顶走到底
+ *              2. 缓存+动态规划:时间复杂度O(N),空间复杂度O(N)
+ *              3. 动态规划+数据压缩:时间复杂度O(N),空间复杂度O(1)
+ *              4. 矩阵乘法:时间复杂度O(logN)，空间复杂度O(1)
  */
 #include <vector>
 
-// 暴力递归
+using namespace std;
+
+// 暴力递归, 从底到顶
+int climbStairs(int n) {
+    if (n <= 0) return 0;
+
+    return process(n, n);
+}
+
+int process(int index, int n) {
+    if (index == 1) return 1;
+    if (index == 2) return 2;
+    if (index <= 0) return 0;
+
+    return process(index - 1, n) + process(index - 2, n);
+}
+
+// 暴力递归, 从顶到底
 int climbStairs(int n) {
     if (n <= 0) return 0;
 
@@ -13,42 +36,42 @@ int climbStairs(int n) {
 }
 
 int process(int index, int n) {
-    if (index == n) return 0;
-    if (index > n) return -1;
+    if (index >= n) return 0;
+    if (index == n - 2) return 2;
+    if (index == n - 1) return 1;
 
-    int ans = 0;
-    if (index + 1 < n) ans += process(index + 1, n) + 1;
-    if (index + 2 < n) ans += process(index + 2, n) + 1;
-    return ans;
+    return process(index + 1, n) + process(index + 2, n);
 }
 
-// 缓存+动态规划
+// 缓存+动态规划(从底到顶实现)
 int climbStairs(int n) {
     if (n <= 0) return 0;
 
-    std::vector<int> dp(n+1);
-    dp[n] = 0;
-    for (int i = n - 1; i <= 0; i--) {
-        int ways = 0;
-        if (i + 1 < n) ways += dp[i+1] + 1;
-        if (i + 2 < n) ways += dp[i+2] + 1;
-        dp[i] = ways;
+    std::vector<int> dp(n+2);
+    dp[1] = 1, dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
     }
-    return dp[0];
+    return dp[n];
 }
 
 // 数据压缩
 int climbStairs(int n) {
     if (n <= 0) return 0;
 
-    int cur = 0, pre = 1, prepre = 2;
-    for (int i = n - 1; i <= 0; i--) {
-        int ways = 0;
-        if (i + 1 < n) ways += cur + 1;
-        if (i + 2 < n) ways += prepre + 1;
-        cur = ways;
-        pre = cur;
+    int cur = 1, pre = 1, prepre = 0;
+    for (int i = 1; i <= n; i++) {
+        cur = pre + prepre;
         prepre = pre;
+        pre = cur;
     }
     return cur;
+}
+
+int climbStairs(int n) {
+
+}
+
+vector<vector<int> > matrixPower() {
+
 }
---
+++ algorithm/CPP/topic/DoublePointer/L26_RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file L26_RemoveDuplicatesFromSortedArray.cpp
+ * @author your name (you@domain.com)
+ * @brief   给你一个升序排列的数组nums，请你原地删除重复出现的元素
+ *          使每个元素只出现一次,返回删除后数组的新长度
+ *          元素的相对顺序应该保持一致 。
+ *          不要使用额外的空间，你必须在原地修改输入数组并在O(1)额外空间的条件下完成。
+ *          链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ */
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+
+}
