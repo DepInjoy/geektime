6f40b267c444ad5abbc09817a4424651b47a7d46
DepInjoy <xieyue28@163.com>
2022-04-27 00:15:30 +0800

    练习题

---
+++ algorithm/CPP/topic/DoublePointer/L259_ThreeSumSmaller.cpp
@@ -0,0 +1,15 @@
+/**
+ * @brief   定一个长度为 n 的整数数组和一个目标值 target
+ *          寻找能够使条件 nums[i] + nums[j] + nums[k] < target成立的三元组
+ *          i, j, k 个数（0 <= i < j < k < n）。
+ *          
+ *          链接：https://leetcode-cn.com/problems/3sum-smaller
+ * 
+ */
+#include <vector>
+
+using namespace std;
+
+int threeSumSmaller(vector<int>& nums, int target) {
+
+}
---
+++ algorithm/CPP/topic/DoublePointer/L31_NextPermutation.cpp
@@ -0,0 +1,17 @@
+/**
+ * @brief   整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。
+ *          更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，
+ *          那么数组的 下一个排列 就是在这个有序容器中排在它后面的那个排列。
+ *          如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（
+ *          即，其元素按升序排列）
+ *          
+ *          链接：https://leetcode-cn.com/problems/next-permutation/
+ * 
+ */
+#include <vector>
+
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+
+}
