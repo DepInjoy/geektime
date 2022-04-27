3538ecbd449f1247a41ea9166822a99c4c1233c4
DepInjoy <xieyue28@163.com>
2022-04-27 18:09:37 +0800

    1二分查找

--- algorithm/CPP/topic/Binaryearch/L69_MySqrt.cpp
+++ algorithm/CPP/topic/Binaryearch/L69_MySqrt.cpp
@@ -1,9 +1,23 @@
 /**
- * @brief   给你一个非负整数 x ，计算并返回 x 的 算术平方根
- *          由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 
+ * @brief   给你一个非负整数x ，计算并返回x的算术平方根
+ *          由于返回类型是整数，结果只保留整数部分 ，小数部分将被舍去 
  * 
  */
 
-    int mySqrt(int x) {
+int mySqrt(int x) {
+   if (x < 0) return -1;
 
-    }
+   int l = 0, r = x, mid = (l + r) >> 1;
+   while (l < r) {
+      mid = (l + r) >> 1;
+      if (mid * mid > x) {
+         r = mid - 1;
+      } else if (mid * mid < x) {
+         if((mid+1) * (mid+1) > x) return mid;
+         l = mid;
+      } else {
+         return mid;
+      }
+   }
+   return mid;
+}
