87096e792f972c7843b551d3306098a0d08526da
DepInjoy <xieyue28@163.com>
2022-04-28 23:06:30 +0800

    1DP

--- algorithm/CPP/topic/DP/L746_MinCostClimbingStairs.cpp
+++ algorithm/CPP/topic/DP/L746_MinCostClimbingStairs.cpp
@@ -4,8 +4,13 @@
  *          你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
  *          请你计算并返回达到楼梯顶部的最低花费
  *          链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
+ * 
+ *          实现思想：
+ *              1. 动态规划+记忆化搜索
+ *              2. 空间压缩
  *  
  */
+
 #include <vector>
 #include <algorithm>
 #include <cmath>
@@ -13,5 +18,22 @@
 using namespace std;
 
 int minCostClimbingStairs(vector<int>& cost) {
+    int N = cost.size();
+    vector<int> dp = vector<int>(N+1);
+    dp[0] = dp[1] = 0;
+    for (int i = 2; i <= N; i++) {
+        dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+    }
+    return dp[N];
+}
 
+// 空间压缩
+int minCostClimbingStairs(vector<int>& cost) {
+    int cur = 0, pre = 0, prepre = 0;
+    for (int i = 2; i <= cost.size(); i++) {
+        cur = min(pre+cost[i-1], prepre+cost[i-2]);
+        prepre = pre;
+        pre = cur;
+    }
+    return cur;
 }
