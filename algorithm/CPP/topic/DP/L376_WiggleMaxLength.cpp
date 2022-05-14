dcc9a9351d15a133342d38776da03c8db420a185
DepInjoy <xieyue28@163.com>
2022-05-14 18:39:04 +0800

    2DP+1贪心

---
+++ algorithm/CPP/topic/DP/L376_WiggleMaxLength.cpp
@@ -0,0 +1,60 @@
+/**
+ * @brief   376. 摆动序列
+ *          如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。
+ *          第一个差（如果存在的话）可能是正数或负数。仅有一个元素或者含两个不等元素的序列也视作摆动序列。
+ *          例如， [1, 7, 4, 9, 2, 5] 是一个 摆动序列 ，因为差值 (6, -3, 5, -7, 3) 是正负交替出现的。
+ *          相反，[1, 4, 7, 2, 5] 和 [1, 7, 4, 5, 5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数
+ *          第二个序列是因为它的最后一个差值为零。
+ * 
+ *          子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素保持其原始顺序。
+ *          给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度 
+ * 
+ *          链接：https://leetcode.cn/problems/wiggle-subsequence
+ *           
+ *          实现思想：
+ *              1. 动态规划，时间复杂度O(N), 空间复杂度O(N)
+ *              2. 动态规划+滑动数组,时间复杂度O(N), 空间复杂度O(1)
+ *              3. 贪心
+ *          
+ *          思想见题解：https://leetcode.cn/problems/wiggle-subsequence/solution/bai-dong-xu-lie-by-leetcode-solution-yh2m/
+ */
+
+// 动态规划
+int wiggleMaxLength(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> up(n), down(n);
+    up[0] = 1, down[0] = 1;
+    for (int i = 1; i < n; i++) {
+        if (nums[i-1] < nums[i]) {
+            up[i] = max(down[i-1]+1, up[i-1]);
+            down[i] = down[i-1];
+        } else if (nums[i-1] > nums[i]) {
+            up[i] = up[i-1];
+            down[i] = max(up[i-1]+1, down[i-1]);
+        } else {
+            up[i] = up[i-1];
+            down[i] = down[i-1];
+        }
+    }
+    return max(up[n-1], down[n-1]);
+}
+
+// 动态规划+滑动数组
+int wiggleMaxLength(vector<int>& nums) {
+    int n = nums.size();
+    int upPre = 1, upCur=1, downPre = 1, downCur=1;
+    for (int i = 1; i < n; i++) {
+        if (nums[i-1] < nums[i]) {
+            upCur = max(downPre+1, upPre);
+            downCur = downPre;
+        } else if (nums[i-1] > nums[i]) {
+            upCur = upPre;
+            downCur = max(upPre+1, downPre);
+        } else {
+            upCur = upPre;
+            downCur = downPre;
+        }
+        upPre = upCur, downPre = downCur;
+    }
+    return max(upCur, downCur);
+}
