12039b3e735ffe615cf35119a3d2b90d86615b96
DepInjoy <xieyue28@163.com>
2022-06-10 23:08:44 +0800

    更新URLURL化

--- algorithm/CPP/Practice/InterviewClassic/IC_01.03_StringUoUrl.cpp
+++ algorithm/CPP/Practice/InterviewClassic/IC_01.03_StringUoUrl.cpp
@@ -1,5 +1,8 @@
 /**
  * @brief   01.03. URL化
+ *          URL化。编写一种方法，将字符串中的空格全部替换为%20。
+ *          假定该字符串尾部有足够的空间存放新增字符，并且知道字符串的“真实”长度
+ *          （注：用Java实现的话，请使用字符数组实现，以便直接在数组上操作。）
  *          链接：https://leetcode.cn/problems/string-to-url-lcci/
  *          
  *          实现思想：
@@ -26,17 +29,19 @@ string replaceSpaces(string S, int length) {
     return ans;
 }
 
-//  动态扩容，超出时间限制，应该有优化空间
+// 题目假定字符串尾部有足够的空间存放新增字符
 string replaceSpaces(string S, int length) {
-    string ans(S.substr(0, length));
-    int index = 0;
-    for (int i = length-1; i >= 0; i--) {
-        if (S[i] == ' ') {
-            ans[i] = '0';
-            ans.insert(i, "%2");
+    string ans(S);
+    int i = S.size()-1, j = length -1;
+    while(j >= 0) {
+        if (S[j] == ' ') {
+            ans[i--] = '0', ans[i--] = '2', ans[i--] = '%';
+        } else {
+            ans[i--] = S[j];
         }
+        j--;
     }
-    return ans;
+    return ans.substr(i+1, S.size());
 }
 
 string replaceSpaces(string S, int length) {
