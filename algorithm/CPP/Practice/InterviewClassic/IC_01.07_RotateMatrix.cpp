efc2cd18e3bfd5f1a21c2c13a743b5b82276cdd8
DepInjoy <xieyue28@163.com>
2022-06-15 07:41:48 +0800

    旋转旋转矩阵矩阵

--- algorithm/CPP/Practice/InterviewClassic/IC_01.07_RotateMatrix.cpp
+++ algorithm/CPP/Practice/InterviewClassic/IC_01.07_RotateMatrix.cpp
@@ -5,3 +5,23 @@
  *          不占用额外内存空间能否做到？
  *          链接：https://leetcode.cn/problems/rotate-matrix-lcci/
  */
+
+void rotate(vector<vector<int>>& matrix) {
+    if (matrix.size() == 0 || matrix.size() != matrix[0].size()) return;
+
+    int N = matrix.size();
+    for (int layer = 0; layer <= N/2; ++layer) {
+        int start = layer, end = N-1-start;
+        for (int i = start; i < end; ++i) {
+            int offset = i - start;
+            int top = matrix[start][i];
+            // 左下角移动到上边
+            matrix[start][i] = matrix[end - offset][start];
+            // 右下角移动到左边
+            matrix[end - offset][start] = matrix[end][end-offset];
+            // 右上到下
+            matrix[end][end-offset] = matrix[i][end];
+            matrix[i][end] = top;
+        }
+    }
+}
