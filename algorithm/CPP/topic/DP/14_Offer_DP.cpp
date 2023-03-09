5e397b94b47459c59fc0ac279f69fdebbf17eb1a
DepInjoy <xieyue28@163.com>
2023-03-09 22:03:17 +0800

    算法练习

--- algorithm/CPP/topic/DP/14_Offer_DP.cpp
+++ algorithm/CPP/topic/DP/14_Offer_DP.cpp
@@ -172,3 +172,27 @@ int lengthOfLongestSubstring(string s) {
     std::vector<int> arr(s.size());
     
 }
+
+/**
+ * @brief   剑指 Offer 14-I. 剪绳子
+ *              给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+ *              每段绳子的长度记为 k[0],k[1]...k[m-1]. 请问 k[0]*k[1]*...*k[m-1]可能的最大乘积是多少？
+ *              例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18
+ * 
+ *          https://leetcode.cn/problems/jian-sheng-zi-lcof/description
+ */
+
+/**
+ * @brief   dp[i] 表示将正整数 iii 拆分成至少两个正整数的和之后，这些正整数的最大乘积
+ * 
+ */
+int cuttingRope(int n) {
+    std::vector<int> products(n+1, 0);
+    products[0]= 0, products[1] = 0;
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; j < i; ++j) {
+            products[i] = std::max(products[i], std::max(j *(i - j), j * products[i - j]));;
+        }
+    }
+    return products[n];
+}
