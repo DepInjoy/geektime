1eca1be96134d456bb22ec2d87e91c46a6d4df6a
DepInjoy <xieyue28@163.com>
2023-04-11 07:41:02 +0800

    DP练习

--- algorithm/CPP/topic/DP/14_Offer_DP.cpp
+++ algorithm/CPP/topic/DP/14_Offer_DP.cpp
@@ -223,3 +223,57 @@ int minCostClimbingStairs(vector<int>& cost) {
     }
     return cur;
 }
+
+/**
+ * 专题 剑指 Offer-II 089. 房屋偷盗(中等)
+ *      https://leetcode.cn/problems/Gu0c2T/description/
+ * 
+ *      一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+ *      影响小偷偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ *      如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ *      给定一个代表每个房屋存放金额的非负整数数组 nums
+ *      请计算 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ *      
+ *      dp[i]表示第一间房屋可以偷窃到的最高金额
+ *      状态转移方程: dp[i] = max(dp[i-1] + nums[i], dp[i-1]) 
+ *      初始化状态 dp[0] = num[0], dp[1] = max(nums[0], nums[1])
+*/
+int rob(vector<int>& nums) {
+    int prepre = 0, pre = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+        int cur = std::max(prepre + nums[i], pre);
+        prepre = pre;
+        pre = cur;
+    }
+    return pre;
+}
+
+/**
+ *      剑指 Offer II 090. 环形房屋偷盗
+ *      https://leetcode.cn/problems/PzWKhm/description/
+ *      https://leetcode-cn.com/problems/house-robber-ii/
+ * 
+ *      一个专业的小偷，计划偷窃一个环形街道上沿街的房屋，每间房内都藏有一定的现金。
+ *      这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。
+ *      同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ *      给定一个代表每个房屋存放金额的非负整数数组 nums ，请计算 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+*/
+int rob(vector<int>& nums) {
+    // 偷第一个,不偷最后一个
+    int prepre = 0, pre = nums[0];
+    for (int i = 1; i < nums.size() - 1; ++i) {
+        int cur = std::max(prepre + nums[i], pre);
+        prepre = pre;
+        pre = cur;
+    }
+
+    // 不偷第一个, 偷最后一个
+    int prepre2 = 0, pre2 = 0;
+    for (int i = 1; i < nums.size(); ++i) {
+        int cur = std::max(prepre2 + nums[i], pre2);
+        prepre2 = pre2;
+        pre2 = cur;
+    }
+    return std::max(pre, pre2);
+}
+
