3166f9e10b39ad313dfd25b30e678d47008292a7
DepInjoy <xieyue28@163.com>
2023-04-13 07:32:57 +0800

    二分查找练习

---
+++ algorithm/CPP/topic/Binaryearch/11_Offer_BinarySearch.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+using namespace std;
+/**
+*   剑指 Offer II 068. 查找插入位置
+*   https://leetcode.cn/problems/N6YdxV/description/
+*   
+*   给定一个排序的整数数组nums和一个整数目标值 target, 请在数组中找到target,
+*   并返回其下标。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+*/
+int searchInsert(vector<int>& nums, int target) {
+    int l = 0, r= nums.size() - 1;
+    int ans = nums.size();
+    while (l <= r) {
+        int mid = ((r - l) >> 1) + l;
+        if (nums[mid] >= target) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1; 
+        }
+    }
+    return ans;
+}
+
+/**
+ * 剑指 Offer II 069. 山峰数组的顶部
+ *      https://leetcode.cn/problems/B1IidL/description/
+ *      https://leetcode-cn.com/problems/peak-index-in-a-mountain-array/
+*/
+int peakIndexInMountainArray(vector<int>& arr) {
+    int n = arr.size();
+    int l = 0, r = n - 1;
+    int ans = 0;
+    while (l <= r) {
+        int mid = ((r - l) >> 1) + l;
+        if (arr[mid] > arr[mid+1]) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
