54a41f6d0c603f42ff40a6813aff92ab399ed2b1
DepInjoy <xieyue28@163.com>
2024-01-09 22:54:10 +0800

    algorithm quick sort

--- doc/Algorithm/General/04_Heap/347_LC_M_top-k-frequent-elements.cpp
+++ doc/Algorithm/General/04_Heap/347_LC_M_top-k-frequent-elements.cpp
@@ -3,34 +3,76 @@
  * https://leetcode.cn/problems/top-k-frequent-elements/description
 */
 
-static bool cmp(std::pair<int, int>& l, std::pair<int, int>& r) {
-    return r.second < l.second;
-};
+// ---------------------- 快排 + Hash表 ---------------------------
+bool cmp(const std::pair<int, int>& l, const std::pair<int, int>& r) {
+    return l.second > r.second;
+}
+
+void quick_sort(std::vector<std::pair<int, int>>& data, int l , int r, int k) {
+    if (l >= r) return;
+    
+    std::pair<int, int> x = data[rand() % (r - l + 1) + l];
+    int i = l - 1, j = r + 1;
+    while (i < j) {
+        while(cmp(data[++i], x));
+        while(cmp(x, data[--j]));
+        if (i < j) std::swap(data[i],data[j]);
+    }
+
+    if (j -l + 1 > k) {
+        quick_sort(data, l, j, k);
+    } else {
+        quick_sort(data, j + 1, r, k - (j - l + 1));
+    }
+}
 
 vector<int> topKFrequent(vector<int>& nums, int k) {
-    std::unordered_map<int, int> ump;
+    const int n = nums.size();
+    std::unordered_map<int, int> ump(n);
+    for (int num : nums) {
+        ump[num]++;
+    }
+
+    const int dn = ump.size();
+    std::vector<std::pair<int, int>> data(dn);
+    int i = 0;
+    for (auto& [val, cnt] : ump) {
+        data[i++] = std::make_pair<>(val, cnt);
+    }
+
+    quick_sort(data, 0, dn - 1, k);
+    std::vector<int> ans(k);
+    for (int i = 0; i < k; i++) {
+        ans[i] = data[i].first;
+    }
+    return ans;
+}
+// ---------------------- 快排 + Hash表 ---------------------------
+
+// 堆 + hash表
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    auto cmp = [](const std::pair<int, int>& l, const std::pair<int, int>& r) {
+        return l.second > r.second;
+    };
+    
+    const int n = nums.size();
+    std::unordered_map<int, int> ump(n);
     for (int num : nums) {
         ++ump[num];
     }
 
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(&cmp)> prio(cmp);
-
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(cmp)> heap(cmp);
     for (auto& [num, cnt] : ump) {
-        if (prio.size() == k) {
-            if (prio.top().second < cnt) {
-                prio.pop();
-                prio.emplace(num, cnt);
-            }
-        } else {
-            prio.emplace(num, cnt);
+        heap.emplace(num, cnt);
+        if (heap.size() > k) {
+            heap.pop();
         }
     }
 
-    std::vector<int> ans(prio.size());
-    int i = 0;
-    while (!prio.empty()) {
-        ans[i++] = prio.top().first;
-        prio.pop();
+    std::vector<int> ans(heap.size());
+    for (int i = heap.size() - 1; i >= 0; --i) {
+        ans[i] = heap.top().first;
+        heap.pop();
     }
     return ans;
 }
--- doc/Algorithm/General/Topic.md
+++ doc/Algorithm/General/Topic.md
@@ -3,8 +3,8 @@
 2. [122. 买卖股票的最佳时机 II](https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/description)    动态规划+空间压缩 [相关实现](02_DP\122_LC_M_best-time-to-Buy-And-Sell-Stock-ii.cpp)
 
 # 堆
- 1. [347. 前 K 个高频元素](https://leetcode.cn/problems/top-k-frequent-elements/description) [相关实现](04_Heap/347_LC_M_top-k-frequent-elements.cpp)  堆 + Hash表
-2. [692. 前K个高频单词](https://leetcode.cn/problems/top-k-frequent-words/description) [相关实现](./04_Heap/692_LC_M_top-k-frequent-words.cpp) 堆+Hash表
+ 1. [347. 前 K 个高频元素](https://leetcode.cn/problems/top-k-frequent-elements/description) [相关实现](04_Heap/347_LC_M_top-k-frequent-elements.cpp)  堆/快排+ Hash表
+2. [692. 前K个高频单词](https://leetcode.cn/problems/top-k-frequent-words/description) [相关实现](./04_Heap/692_LC_M_top-k-frequent-words.cpp) 堆/快排+Hash表
 
 # 单调栈
 1. [496. 下一个更大元素 I](https://leetcode.cn/problems/next-greater-element-i/description/)       [相关实现](02_MonotonicStack/496_E_Next-Greater-Element-i.cpp)      单调栈
@@ -14,3 +14,7 @@
 
 # Hash表
 1. [1. 两数之和](https://leetcode.cn/problems/two-sum/description/) [相关实现](09_Hash/01_LC_E_two-sum.cpp)
+
+# 排序
+1. [347. 前 K 个高频元素](https://leetcode.cn/problems/top-k-frequent-elements/description) [相关实现](04_Heap/347_LC_M_top-k-frequent-elements.cpp)  快排/堆+ Hash表
+2. [692. 前K个高频单词](https://leetcode.cn/problems/top-k-frequent-words/description) [相关实现](./04_Heap/692_LC_M_top-k-frequent-words.cpp) 快排/堆+Hash表
