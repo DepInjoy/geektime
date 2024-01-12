109b4647fd6c9d4154b08266b8815559f9869a69
DepInjoy <dep_injoy@163.com>
2024-01-12 22:00:58 +0800

    algorithm quick sort

--- doc/Algorithm/General/04_Heap/692_LC_M_top-k-frequent-words.cpp
+++ doc/Algorithm/General/04_Heap/692_LC_M_top-k-frequent-words.cpp
@@ -3,9 +3,7 @@
  * https://leetcode.cn/problems/top-k-frequent-words/description
 */
 
-
-
-// 堆+Hash表
+//------------------------- 堆+Hash表 ----------------------------------
 vector<string> topKFrequent(vector<string>& words, int k) {
     const int n = words.size();
     std::unordered_map<std::string, int> ump(n);
@@ -36,3 +34,50 @@ vector<string> topKFrequent(vector<string>& words, int k) {
     }
     return ans;
 }
+// ---------------------------------------------------------------------------
+
+// -------------------------------- Hash表 + 快排 -----------------------------
+using PII = std::pair<std::string, int>;
+bool cmp(const PII& lhs, const PII& rhs) {
+    return lhs.second == rhs.second ? lhs.first < rhs.first : lhs.second > rhs.second;
+}
+
+void quick_sort(std::vector<PII>& datas, int l, int r, int k) {
+    if (l >= r) return;
+
+    PII x = datas[random() % (r - l + 1) + l];
+    int i = l - 1, j = r + 1;
+    while (i < j) {
+        while(cmp(datas[++i], x));
+        while(cmp(x, datas[--j]));
+        if (i < j) {
+            std::swap(datas[i], datas[j]);
+        }
+    }
+
+    quick_sort(datas, l, j, k), quick_sort(datas, j+1, r, k);
+}
+
+std::vector<std::string> topKFrequent(vector<string>& words, int k) {
+    const int n = words.size();
+    std::unordered_map<std::string, int> ump(n);
+    for (auto word : words) {
+        ump[word]++;
+    }
+
+    std::vector<PII> datas;
+    datas.reserve(n);
+    for (auto& [word, cnt] : ump) {
+        datas.push_back({word, cnt});
+    }
+
+    int l = 0, r = datas.size() - 1;
+    quick_sort(datas, l, r, k);
+
+    std::vector<std::string> ans(k);
+    for (int i = 0; i < k; ++i) {
+        ans[i] = datas[i].first;
+    }
+    return ans;
+}
+// ---------------------------------------------------------------------------
