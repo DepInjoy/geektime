79e492e604073427b0149988d779fbc7a29587aa
DepInjoy <xieyue28@163.com>
2024-01-30 00:40:16 +0800

    Algorithm Full Bag DP

--- doc/Algorithm/General/02_DP/518_LC_M_coin-change-ii.cpp
+++ doc/Algorithm/General/02_DP/518_LC_M_coin-change-ii.cpp
@@ -1,5 +1,16 @@
 /**
- * 
- * 
+ * 518. 零钱兑换 II
+ * https://leetcode.cn/problems/coin-change-ii/description/
 */
-coin-change-ii
+
+// 完全背包
+int change(int amount, vector<int>& coins) {
+    std::vector<int> dp(amount+1);
+    dp[0] = 1;
+    for (int i = 0; i < coins.size(); ++i) {
+        for (int j = coins[i]; j <= amount; ++j) {
+            dp[j] += dp[j - coins[i]];
+        }
+    }
+    return dp[amount];
+}
