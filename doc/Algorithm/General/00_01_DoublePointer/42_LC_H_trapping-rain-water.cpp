abe8fb06061dfbbc1dda7a53d32d4604019c339d
DepInjoy <xieyue28@163.com>
2024-05-22 22:56:56 +0800

    Algorithm double Pointer

--- doc/Algorithm/General/00_01_DoublePointer/42_LC_H_trapping-rain-water.cpp
+++ doc/Algorithm/General/00_01_DoublePointer/42_LC_H_trapping-rain-water.cpp
@@ -30,5 +30,37 @@ int trap(vector<int>& height) {
 }
 
 // 单调栈
+int trap(vector<int>& height) {
+    std::stack<int> stk;
+    int ans = 0;
+    for (int i = 0; i < height.size(); ++i) {
+        while (!stk.empty() && height[i] > height[stk.top()]) {
+            int top = stk.top();
+            stk.pop();
+            if (stk.empty()) break;
+            int left = stk.top();
+            int width = i - left - 1;
+            int waterH = std::min(height[i], height[left]) - height[top];
+            ans += width * waterH;
+        }
+        stk.push(i);
+    }
+    return ans;
+}
 
 // 双指针
+int trap(vector<int>& height) {
+    int ans = 0;
+    int leftMaxH = 0, rightMaxH = 0;
+    int l = 0, r = height.size() - 1;
+    while (l < r) {
+        leftMaxH = std::max(leftMaxH, height[l]);
+        rightMaxH = std::max(rightMaxH,  height[r]);
+        if (height[l] < height[r]) {
+            ans += leftMaxH - height[l++];
+        } else {
+            ans += rightMaxH - height[r--];
+        }
+    }
+    return ans;
+}
