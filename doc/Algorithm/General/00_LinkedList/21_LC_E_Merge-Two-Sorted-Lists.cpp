fd95536f6d84dd66f0278afd93e4d2fa8015ed7f
DepInjoy <xieyue28@163.com>
2024-05-27 23:13:00 +0800

    algorithm

--- doc/Algorithm/General/00_LinkedList/21_LC_E_Merge-Two-Sorted-Lists.cpp
+++ doc/Algorithm/General/00_LinkedList/21_LC_E_Merge-Two-Sorted-Lists.cpp
@@ -3,23 +3,17 @@
  * https://leetcode.cn/problems/merge-two-sorted-lists/description/
 */
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    if (!list1) return list2;
-    if (!list2) return list1;
-
-    ListNode* cur1 = list1, *cur2 = list2;
-    ListNode dummy, *cur = &dummy;
-    while (cur1 && cur2) {
-        if (cur1->val <= cur2->val) {
-            cur->next = cur1;
-            cur1 = cur1->next;
+    ListNode dummy, * tail = &dummy;
+    while (list1 && list2) {
+        if (list1->val < list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
         } else {
-            cur->next = cur2;
-            cur2 = cur2->next;
+            tail->next = list2;
+            list2 = list2->next;
         }
-        cur = cur->next;
+        tail = tail->next;
     }
-    while (cur1) cur->next = cur1, cur1 = cur1->next, cur = cur->next;
-    while (cur2) cur->next = cur2, cur2 = cur2->next, cur = cur->next;
-
+    tail->next = list1 ? list1 : list2;
     return dummy.next;
 }
